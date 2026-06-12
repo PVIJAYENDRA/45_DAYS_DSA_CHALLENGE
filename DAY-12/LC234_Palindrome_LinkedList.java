7e045066a6151ff28d106728efc7f86d85890a32
PVIJAYENDRA <vijayendrapathipati@gmail.com>
2026-06-12 21:35:03 +0530

    Day 12: Solved LC142, LC234

---
+++ DAY-12/LC234_Palindrome_LinkedList.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+
+         ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+           ListNode prev = null;
+        while( slow != null){
+            ListNode temp = slow.next;
+            slow.next = prev;
+            prev = slow ;
+            slow = temp;
+        }
+
+        while( prev != null){
+            if(head.val != prev.val){
+                return false;
+            }
+            head = head.next;
+            prev = prev.next;
+        }
+
+        return true;
+        
+    }
+}
