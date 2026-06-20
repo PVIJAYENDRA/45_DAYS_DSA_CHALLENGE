584a0862c300e18b32dc170628c05478cc4eab33
PVIJAYENDRA <vijayendrapathipati@gmail.com>
2026-06-20 15:13:12 +0530

    Day 17: Solved GFG, LC90, LC39, LC40

---
+++ DAY-17/GFG_Subsets Sum.java
@@ -0,0 +1,22 @@
+class Solution {
+   
+    public void findSums(int index, int currentSum, int[] arr, ArrayList<Integer> sums) {
+        if (index == arr.length) {
+            sums.add(currentSum);
+            return;
+        }
+
+        // Include current element
+        findSums(index + 1, currentSum + arr[index], arr, sums);
+
+        // Exclude current element
+        findSums(index + 1, currentSum, arr, sums);
+    }
+
+    public ArrayList<Integer> subsetSums(int[] arr) {
+        ArrayList<Integer> sums = new ArrayList<>();
+        findSums(0, 0, arr, sums);
+        Collections.sort(sums);
+        return sums;
+    }
+}
